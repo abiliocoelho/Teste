18670d7b5f879d420c07b71a0a06304fed7f9e1d
Abilio S Coelho <abiliocoelho@gmail.com>
2023-04-04 07:49:03 -0300

    oks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -69,6 +69,9 @@ export default function Home({ landingPage }: LandinPageProps) {
         <h1 className={inter.className}>
           {landingPage.data.attributes.header.company}
         </h1>
+        <h1 className={inter.className}>
+          {landingPage.data.attributes.header.company}
+        </h1>
       </main>
     </>
   );
